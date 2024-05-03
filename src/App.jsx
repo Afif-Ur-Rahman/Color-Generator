f6470e154de154933d4b78a45ddbaa1caf8b16e4
Afif_Ur_Rahman <afifurrahman444@gmail.com>
2024-05-03 13:24:49 +0500

    Added Color Picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
+import { SketchPicker } from "react-color";
 import Clipboard from "./assets/Clipboard.svg";
 import "./App.css";
 
 const App = () => {
   const [color, setColor] = useState("#ffffff");
   const [isCopied, setIsCopied] = useState(false);
+
+  const handleHexColor = (newColor) => {
+    console.log(newColor);
+    setColor(newColor.hex);
+  };
   const handleColorChange = () => {
     const color = "#" + Math.floor(Math.random() * 16777215).toString(16);
     setColor(color);
@@ -20,27 +26,39 @@ const App = () => {
   return (
     <>
       <div
-        className="flex pt-40 flex-wrap items-center flex-col font-mono w-full h-screen"
+        className="flex justify-center flex-wrap items-center font-mono w-full h-screen"
         style={{ backgroundColor: color }}
       >
-        <div className="bg-white rounded-lg bg-opacity-70 w-max p-4">
-          <h1 className="font-mono text-xl font-bold pb-2">Color Generator</h1>
-          <div className="flex justify-center items-center">
-            <button
-              className="rounded-3xl text-black bg-transparent"
-              onClick={() => handleColorChange()}
-            >
-              {color}
-            </button>
-            <button
-              className="rounded-3xl flex justify-center items-center bg-transparent tooltip"
-              onClick={() => handleCopy()}
-            >
-              <img src={Clipboard} alt="Clipboard Logo" />
-              <span className={`tooltiptext ${isCopied ? "block" : "hidden"}`}>
-                Copied!
-              </span>
-            </button>
+        <div className="flex justify-center items-center">
+          <div className="main flex justify-center items-center bg-white rounded-lg border-2 border-gray-300 bg-opacity-70 w-max p-4">
+            <div className="m-4">
+              <h1 className="font-mono text-xl font-bold pb-2">
+                Random Color Generator
+              </h1>
+              <div className="flex justify-center items-center">
+                <button
+                  className="rounded-3xl text-black bg-transparent"
+                  onClick={() => handleColorChange()}
+                >
+                  {color}
+                </button>
+                <button
+                  className="rounded-3xl flex justify-center items-center bg-transparent tooltip"
+                  onClick={() => handleCopy()}
+                >
+                  <img src={Clipboard} alt="Clipboard Logo" />
+                  <span
+                    className={`tooltiptext ${isCopied ? "block" : "hidden"}`}
+                  >
+                    Copied!
+                  </span>
+                </button>
+              </div>
+            </div>
+            <div className="flex justify-center items-center flex-col m-4">
+              <h1 className="font-mono text-xl font-bold pb-2">Color Picker</h1>
+              <SketchPicker color={color} onChange={handleHexColor} />
+            </div>
           </div>
         </div>
       </div>
