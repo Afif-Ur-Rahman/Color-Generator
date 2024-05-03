1da65141a8dd2cca43e202bd835ce5a84bd49905
Afif_Ur_Rahman <afifurrahman444@gmail.com>
2024-05-03 15:55:51 +0500

    Added RGBA and HSL

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,60 @@
 import { useState } from "react";
 import { SketchPicker } from "react-color";
-import Clipboard from "./assets/Clipboard.svg";
+import tinycolor from "tinycolor2";
+
 import "./App.css";
 
 const App = () => {
-  const [color, setColor] = useState("#ffffff");
-  const [isCopied, setIsCopied] = useState(false);
+  const [hex, setHex] = useState("#ffffff");
+  const [rgba, setRgba] = useState("rgba(255, 255, 255, 1)");
+  const [hsl, setHsl] = useState("hsl(0, 0%, 100%)");
+  const [isCopiedHex, setIsCopiedHex] = useState(false);
+  const [isCopiedRgba, setIsCopiedRgba] = useState(false);
+  const [isCopiedHsl, setIsCopiedHsl] = useState(false);
 
-  const handleHexColor = (newColor) => {
-    console.log(newColor);
-    setColor(newColor.hex);
+  const handleColor = (newColor) => {
+    setHex(newColor.hex);
+    setRgba(
+      `rgba(${newColor.rgb.r}, ${newColor.rgb.g}, ${newColor.rgb.b}, ${newColor.rgb.a})`
+    );
+    setHsl(
+      `hsl(${newColor.hsl.h.toFixed(0)}, ${newColor.hsl.s.toFixed(
+        2
+      )}%, ${newColor.hsl.l.toFixed(2)}%)`
+    );
   };
+
   const handleColorChange = () => {
-    const color = "#" + Math.floor(Math.random() * 16777215).toString(16);
-    setColor(color);
+    const color = tinycolor.random();
+    const rgb = color.toRgb();
+    const rgba = `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, ${rgb.a})`;
+    const hsl = color.toHslString();
+    setHex(color.toHexString());
+    setRgba(rgba);
+    setHsl(hsl);
   };
-  const handleCopy = () => {
-    navigator.clipboard.writeText(color);
-    setIsCopied(true);
+
+  const handleCopyHex = () => {
+    navigator.clipboard.writeText(hex);
+    setIsCopiedHex(true);
     setTimeout(() => {
-      setIsCopied(false);
+      setIsCopiedHex(false);
+    }, 1000);
+  };
+
+  const handleCopyRgba = () => {
+    navigator.clipboard.writeText(rgba);
+    setIsCopiedRgba(true);
+    setTimeout(() => {
+      setIsCopiedRgba(false);
+    }, 1000);
+  };
+
+  const handleCopyHsl = () => {
+    navigator.clipboard.writeText(hsl);
+    setIsCopiedHsl(true);
+    setTimeout(() => {
+      setIsCopiedHsl(false);
     }, 1000);
   };
 
@@ -27,28 +62,56 @@ const App = () => {
     <>
       <div
         className="flex justify-center flex-wrap items-center font-mono w-full h-screen"
-        style={{ backgroundColor: color }}
+        style={{ backgroundColor: hex }}
       >
         <div className="flex justify-center items-center">
-          <div className="main flex justify-center items-center bg-white rounded-lg border-2 border-gray-300 bg-opacity-70 w-max p-4">
-            <div className="m-4">
+          <div className="main flex flex-wrap justify-center items-center bg-white rounded-lg border-2 border-gray-300 bg-opacity-70 w-max p-4">
+            <div className="flex flex-col">
               <h1 className="font-mono text-xl font-bold pb-2">
                 Random Color Generator
               </h1>
-              <div className="flex justify-center items-center">
+              <button
+                className="rounded-3xl text-black bg-transparent w-full"
+                onClick={() => handleColorChange()}
+              >
+                Click to Generate Color
+              </button>
+              <div className="flex justify-center items-center mt-2 flex-wrap flex-col">
                 <button
-                  className="rounded-3xl text-black bg-transparent"
-                  onClick={() => handleColorChange()}
+                  className="rounded-3xl flex justify-center items-center m-1 bg-transparent tooltip w-full"
+                  onClick={() => handleCopyHex()}
                 >
-                  {color}
+                  {hex}
+                  <span
+                    className={`tooltiptext ${
+                      isCopiedHex ? "block" : "hidden"
+                    }`}
+                  >
+                    Copied!
+                  </span>
                 </button>
                 <button
-                  className="rounded-3xl flex justify-center items-center bg-transparent tooltip"
-                  onClick={() => handleCopy()}
+                  className="rounded-3xl flex justify-center items-center m-1 bg-transparent tooltip w-full"
+                  onClick={() => handleCopyRgba()}
                 >
-                  <img src={Clipboard} alt="Clipboard Logo" />
+                  {rgba}
                   <span
-                    className={`tooltiptext ${isCopied ? "block" : "hidden"}`}
+                    className={`tooltiptext ${
+                      isCopiedRgba ? "block" : "hidden"
+                    }`}
+                  >
+                    Copied!
+                  </span>
+                </button>
+                <button
+                  className="rounded-3xl flex justify-center items-center m-1 bg-transparent tooltip w-full"
+                  onClick={() => handleCopyHsl()}
+                >
+                  {hsl}
+                  <span
+                    className={`tooltiptext ${
+                      isCopiedHsl ? "block" : "hidden"
+                    }`}
                   >
                     Copied!
                   </span>
@@ -57,7 +120,7 @@ const App = () => {
             </div>
             <div className="flex justify-center items-center flex-col m-4">
               <h1 className="font-mono text-xl font-bold pb-2">Color Picker</h1>
-              <SketchPicker color={color} onChange={handleHexColor} />
+              <SketchPicker color={hex} onChange={handleColor} />
             </div>
           </div>
         </div>
